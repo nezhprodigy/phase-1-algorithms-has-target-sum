a86b16f7dcc07bf9ae1624c069f3801076761750
alex <alex.musyoki@student.moringaschool.com>
2023-10-25 19:58:14 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
+}  
   // Write your algorithm here
-}
-
 /* 
   Write the Big O time complexity of your function here
 */
